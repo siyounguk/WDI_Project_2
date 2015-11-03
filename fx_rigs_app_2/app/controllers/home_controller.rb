3e3d4b49c36d510475f8a266c9c28f1d4307afde
siyounguk <si.younguk@gmail.com>
2015-11-03 10:32:10 +0000

    about to enter caching to voting system

--- fx_rigs_app_2/app/controllers/home_controller.rb
+++ fx_rigs_app_2/app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @pedals = Pedal.all.to_a.sort_by! { |pedal| 0 - pedal.get_likes.size }.slice(0,10)
   end
 end
--- fx_rigs_app_2/app/models/pedal.rb
+++ fx_rigs_app_2/app/models/pedal.rb
@@ -8,6 +8,4 @@ class Pedal < ActiveRecord::Base
 
   has_and_belongs_to_many :rigs
   has_and_belongs_to_many :users
- 
-
 end
