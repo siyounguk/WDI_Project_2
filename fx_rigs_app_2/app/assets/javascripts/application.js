2390244572d622de5a31a8393b9de94a427418d9
siyounguk <si.younguk@gmail.com>
2015-11-04 13:40:26 +0000

    added foundation gem to app

--- fx_rigs_app_2/app/assets/javascripts/application.js
+++ fx_rigs_app_2/app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require select2
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
