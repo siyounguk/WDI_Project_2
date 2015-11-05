f19a5c5c9921217a94197d6ce0d0e85030bcb361
siyounguk <si.younguk@gmail.com>
2015-11-05 17:11:17 +0000

    fixed genre bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  
+  user = User.find_or_create_by(username: "adminrole-simon", role:"admin")
